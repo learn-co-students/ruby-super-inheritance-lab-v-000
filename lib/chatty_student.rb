8eb12e1e514cdb156c774ecbcaa981a6b240e0ea
Chris Haugen <haugenc@gmail.com>
2016-03-07 21:21:19 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,7 +5,7 @@ class ChattyStudent < Student
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
-  def hello
-    3.times {|i| super}
+  def raise_hand
+    10.times {|i| super}
   end
 end
