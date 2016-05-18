1b2db79ea4cb247fccaffe79a468296f43bf2309
nOraKat147 <n.mori.iphone@gmail.com>
2016-05-18 05:15:18 -0400

    Done.

--- test.rb
+++ test.rb
@@ -1,3 +1,17 @@
+class Student
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+  
+end
+
+#-----------------------------
+
 class ChattyStudent < Student
 
   def hello
@@ -19,3 +33,12 @@ class ChattyStudent < Student
   end
   
 end
+
+#-----------------------------
+
+cs = ChattyStudent.new
+
+cs.hello
+cs.raise_hand
+
+
