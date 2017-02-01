06619f8687bc276a677a2d08c1fea25779f66955
Elias Ventura <eliasventura23@gmail.com>
2017-02-01 19:20:55 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,16 +6,7 @@ class ChattyStudent < Student
 end
 
 def raise_hand
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
-  super
+  10.times do super
+  end
 end
-
 end
