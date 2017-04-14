3bfb1ae89771d2969b67860ed3c3dda084f70037
Aimee Carney <acarney29@gmail.com>
2017-04-14 23:33:20 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,16 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-    super.times_10
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
 
 end
