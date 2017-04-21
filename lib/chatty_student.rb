91351d5f14cb1b2f84b4b0b25f3654f75c3f4465
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-21 22:15:33 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,25 @@
+#####hello
+#returns a greeting phrase plus a long chatty phrase
+#####raise_hand
+#returns the phrase 'Pick me!' ten times.
+
+class ChattyStudent < Student
+
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+
+  def raise_hand
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+  end
+end
