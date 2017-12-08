a191438710feb3b6c38577a1e29db2c8562dc7af
charityyoungblood <youngblood.charity@gmail.com>
2017-12-07 16:51:25 -0800

    added placeholder for hello method in chatty_student.rb

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,11 @@
 require 'pry'
 
 # create ChattyStudent class 
-
+class ChattyStudent
 #create hello method 
-
-# create rais_hand method 
+  def hello 
+    
+  end 
+# create raise_hand method 
+  
+end 
