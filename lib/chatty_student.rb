ae209b070f35593587e9fe29d79cc285cfa94fa2
charityyoungblood <youngblood.charity@gmail.com>
2017-12-07 17:11:00 -0800

    All tests passing

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,13 +1,22 @@
 require 'pry'
-require './student.rb'
+
 
 # create ChattyStudent class 
-class ChattyStudent
+class ChattyStudent < Student
+  def initialize
+    
+  end
 #create hello method 
-#  def hello 
-#    super 
-#    
-#  end 
+  def hello 
+    super 
+    
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+  end 
 # create raise_hand method 
+  def raise_hand
+    super 
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+   
+  end
   
 end 
--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,11 @@
 class Student 
+  
   def hello 
-    "Hey there! I'm so excited to learn stuff.\n"
+   puts "Hey there! I'm so excited to learn stuff.\n"
   end 
   
   def raise_hand
-    
+    puts "Pick me!\n"
   end
-  
-  
   
 end 
