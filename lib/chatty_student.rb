2e77b390b2af02e903b2f8e20f8d4b6e2edf8e20
charityyoungblood <youngblood.charity@gmail.com>
2017-12-07 16:53:21 -0800

    added 'super' placeholder in hello method, added require student.rb in chatty_student.rb

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,9 +1,11 @@
 require 'pry'
+require './student.rb'
 
 # create ChattyStudent class 
 class ChattyStudent
 #create hello method 
   def hello 
+    super 
     
   end 
 # create raise_hand method 
