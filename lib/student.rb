32794fba46747d31f9ad4de8afc6de939aa35009
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-01-03 01:56:56 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Student
+
+  def hello
+    #returns a greeting phrase
+    puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    # returns the phrase 'Pick me!
+    puts "Pick me!\n"
+  end
+
+end
