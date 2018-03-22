471e97be21a3119e0bf1f623b69fbc7ee647d88a
Maverick Tesini <mavericktesini@gmail.com>
2018-03-22 18:01:55 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+class Student
+  # attr_accessor :first_name, :last_name
+  #
+  # def initialize
+  #
+  # end
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    puts "Pick me!\n"
+  end
+
+end
