bf63cd538b144e200d92c4586f892c9bf89d039c
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-29 18:54:58 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,16 @@
+
+class ChattyStudent < Student
+
+  def hello
+    super
+    chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts chatty_phrase
+  end
+
+  def raise_hand
+    super
+    9.times do
+      puts @phrase
+    end
+  end
+end
