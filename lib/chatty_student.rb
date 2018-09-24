6155532e14a2ed57fd9d2761e8a268a14f232d9b
kvdoherty <kvdoherty@gmail.com>
2018-09-24 23:58:42 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,12 +1,14 @@
-class ChattyStudent
+class ChattyStudent < Student
 
 def hello 
-  puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+  super 
+  puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
 
 def raise_hand
+  super
   i = 0
-  until i == 10
+  until i == 9
     i += 1
     puts "Pick me!"
   end
